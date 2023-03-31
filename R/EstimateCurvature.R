33d70f4eb9f3db168fd7b451d5bfbb05c11356d5
SteveJWR <steven.wilkins.reeves@gmail.com>
2023-03-31 14:20:04 -0700

    update the set level curvature estimates

--- R/EstimateCurvature.R
+++ R/EstimateCurvature.R
@@ -314,29 +314,31 @@ filter_indices <- function(D,y,z,m, tri.const = sqrt(2)){
 #' @export
 #'
 EstimateKappaSet <- function(D,y,z,m,x.set){
+  if(length(x.set) == 0){
+    return(NULL)
+  }
   d1 = nrow(D)
   d2 = ncol(D)
-  if(d1 > d2){
+  if (d1 > d2) {
     D = t(D)
   }
-
-  kappa.set <- rep(NA,length(x.set))
-
-  for(i in seq(length(x.set))){
+  kappa.set <- rep(NA, length(x.set))
+  for (i in seq(length(x.set))) {
     x = x.set[i]
-    dxy = D[y,x]
-    dxz = D[z,x]
-    dyz = D[y,z]
-    dxm = D[m,x]
-    d.vec = c(dxy,dxz,dyz,dxm)
-    if(any(d.vec == Inf)){
+    dxy = D[y, x]
+    dxz = D[z, x]
+    dyz = D[y, z]
+    dxm = D[m, x]
+    d.vec = c(dxy, dxz, dyz, dxm)
+    if (any(d.vec == Inf)) {
       kappa.hat.x <- NA
-    } else{
-      kappa.hat.x <- EstimateKappa(dxy,dxz,dyz,dxm)
+    }
+    else {
+      kappa.hat.x <- EstimateKappa(dxy, dxz, dyz, dxm)
     }
     kappa.set[i] = kappa.hat.x
   }
-
+  return(kappa.set)
   return(kappa.set)
 }
 
@@ -361,41 +363,26 @@ EstimateCurvature <- function(D,
                               verbose = F){
 
 
-  mid.search <- optimal_midpoint_search(D,top.k = 10,
-                                        d.yz.min = min(d.yz.min, max(D)/2),
-                                        d.yz.max = d.yz.max)
-  if(verbose){
+  mid.search <- optimal_midpoint_search(D, top.k = J, d.yz.min = min(d.yz.min,
+                                                                     max(D)/2), d.yz.max = d.yz.max)
+  if (verbose) {
     print("Midpoints: ")
-    max.row <- min(3,nrow(mid.search))
-    print(mid.search[1:max.row,])
+    max.row <- min(3, nrow(mid.search))
+    print(mid.search[1:max.row, ])
   }
-
-  y.opt = mid.search[1,1]
-  z.opt = mid.search[1,2]
-  m.opt = mid.search[1,3]
-
-  opt.vec <- c(y.opt,z.opt,m.opt)
-  if(!any(is.na(opt.vec))){
-    x.set <- filter_indices(D,
-                              y.opt,
-                              z.opt,
-                              m.opt,
-                              tri.const = tri.const)
-
-    kappa.set <- EstimateKappaSet(D,
-                                  y.opt,
-                                  z.opt,
-                                  m.opt,
+  y.opt = mid.search[1, 1]
+  z.opt = mid.search[1, 2]
+  m.opt = mid.search[1, 3]
+  opt.vec <- c(y.opt, z.opt, m.opt)
+  if (!any(is.na(opt.vec))) {
+    x.set <- filter_indices(D, y.opt, z.opt, m.opt, tri.const = tri.const)
+    kappa.set <- EstimateKappaSet(D, y.opt, z.opt, m.opt,
                                   x.set)
-
-    out.set <- list("kappas" = kappa.set,
-                    "kappa.med" = median(kappa.set, na.rm = T),
-                    "midpoints" = mid.search)
-
-  } else {
-    out.set <- list("kappas" = NULL,
-                    "kappa.med" = NULL,
-                    "midpoints" = mid.search)
+    out.set <- list(kappas = kappa.set, kappa.med = median(kappa.set,
+                                                           na.rm = T), midpoints = mid.search)
+  }
+  else {
+    out.set <- list(kappas = NULL, kappa.med = NULL, midpoints = mid.search)
   }
   return(out.set)
 }
